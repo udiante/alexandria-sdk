3793bfbb3efbd381df8e7ec245543016cca7ba7c
Alejandro Quibus <udiante@gmail.com>
2018-11-03 18:48:15 +0100

    Added string format

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 module.exports.Functions = require('./src/Functions')
 module.exports.RequestManager = require('./src/RequestManager')
-
-
-module.exports.test = function(){
-    console.log('TEST')
-}
+module.exports.StringFormat = require('./src/StringFormat')
