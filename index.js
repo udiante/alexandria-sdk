7411f8d2a45cb83efa93d15e765cbf1e76e08e3d
Alejandro Quibus <udiante@gmail.com>
2019-07-06 20:01:37 +0200

    Version 1.1.1: - Added new utils for the APL detection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.Functions = require('./src/Functions')
 module.exports.RequestManager = require('./src/RequestManager')
 module.exports.StringFormat = require('./src/StringFormat')
+module.exports.Utils = require('./src/Utils')
