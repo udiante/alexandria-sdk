3b00063ba5da6f462d30fad61fe6abaeea342070
Alejandro Quibus <udiante@gmail.com>
2019-07-07 15:53:43 +0200

    Refactor APL dependency to a new npm

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports.StringFormat = require('./src/StringFormat')
 // Alexa Exports
 module.exports.AlexaResponseHandler = require('./src/Alexa/AlexaResponseHandler')
 module.exports.AlexaUtils = require('./src/Alexa/AlexaUtils')
-module.exports.AlexaAPLCatalog = require('./src/Alexa/AlexaAPLCatalog')
 module.exports.Utils = require('./src/Alexa/AlexaUtils') //Deprecated
 
 // Alexandria API Exports
--- src/Alexa/AlexaResponseHandler.js
+++ src/Alexa/AlexaResponseHandler.js
@@ -6,12 +6,21 @@ const AlexaUtils = require('./AlexaUtils')
 var ResponseModel = {
     speechOut: '', // Speech to be read as a response
     reprompt: '', // Reprompt
+    shouldEndSession: false, //If true the session will end
+    sessionAttributes: {
+
+    },
     APL: { // Alexa Presentation Lenguage properties
         document: undefined, //APL Document to be rendered 
         dataSources: {} //APL DataSources to be used with the document
     },
+    CARD : { // Card data
+        title : undefined, //String
+        description : undefined, //String
+        smallMediaUrl: undefined, //String
+        largeMediaUrl: undefined, //String
+    },
     properites: {
-        shouldEndSession: false, //If true the session will end
         shouldRepromptInTheSpeech: false //If true the reprompt will is sended in the scpeechOut with a delay.
     },
     hasValidReprompt: function () {
@@ -22,7 +31,7 @@ var ResponseModel = {
     }
 }
 
-module.exports.responseModel = ResponseModel
+module.exports.ResponseModel = ResponseModel
 
 /**
  * @returns {ResponseModel} A valid response model to work with Response Handler
@@ -68,4 +77,5 @@ module.exports.buildResponse = function (handlerInput, responseObject) {
         });
     }
 
+    return responseHandler.getResponse()
 }
